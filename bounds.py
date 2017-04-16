0a4838cc1dfc007c4a27422eb2e7e1e3ab492269
Mason M. Thompson <mmthompson@cal.berkeley.edu>
2017-04-16 14:26:48 -0700

    Add files via upload
    Program accepts 2 user inputs that act as lower and upper bounds. 
    Program considers bounds as inclusive. 
    Bounds are then run through a loop, where they are multiplied by [1-20). 
    If a result (outcome of a bound * a number [1-20) is NOT divisible by 2 with no remainder: console prints 'buzz'
    if a result (outcome of a bound * a number [1-20) IS divisible by 2 with no remainder: console prints 'fizz'

---
+++ bounds.py
@@ -0,0 +1,34 @@
+
+def fizzBuzz():
+    result = (i*multiplier) 
+    if result % 2 == 0:
+        print ("fizz")
+    if result % 2 !=0:
+        print ("buzz")
+
+
+lb = int(input("Please enter your lower-bound integer: "))
+ub = int(input("Please enter your upper-bound integer: "))
+
+#stores user input
+    
+verif = input("\nYou have entered {0} as your lower-bound integer, and {1} as your upper-bound \ninteger. \n\nIf you are content with this sequence, do you wish to proceed? (y/n): ".format(lb,ub))
+response = verif.lower()
+if lb > ub:
+    print ("\nFATAL ERROR: \n\nYour lower-bound input is greater than your upper-bound input. \n\nPlease try again.")
+
+
+#returns stored input to user, prompts user for response to exe program. User response is made lower-case
+
+if verif =="y":
+    for multiplier in range(lb, (ub+1)):
+        for i in range(1,20):
+            print (i, "x", multiplier, "=", i*multiplier)
+            fizzBuzz()
+        
+if verif != "y":
+    print("invalid response, \ntry again.")
+
+
+
+
